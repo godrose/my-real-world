283721ada12dfee261c08486eda62541f49449fc
Gennady Verdel <godrose@gmail.com>
2019-11-24 13:32:49 +0200

    feat(favorites): added favorites feature

---
+++ ManualConduit/Features/Articles/ArticleEnvelope.cs
@@ -0,0 +1,14 @@
+﻿using ManualConduit.Domain;
+
+namespace ManualConduit.Features.Articles
+{
+    public class ArticleEnvelope
+    {
+        public Article Article { get; }
+
+        public ArticleEnvelope(Article article)
+        {
+            Article = article;
+        }
+    }
+}
---
+++ ManualConduit/Features/Articles/ArticleExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using ManualConduit.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace ManualConduit.Features.Articles
+{
+    public static class ArticleExtensions
+    {
+        public static IQueryable<Article> GetAllData(this DbSet<Article> articles)
+        {
+            return articles
+                .Include(x => x.Author)
+                .Include(x => x.ArticleFavorites)
+                .Include(x => x.ArticleTags)
+                .AsNoTracking();
+        }
+    }
+}
